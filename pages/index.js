c90329d4959791a6bbe52a73a6367b0da30dace1
seangirado <seangirado@gmail.com>
2025-08-19 14:27:09 +0800

    third commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello, my name is Sean Kurtley J. Girado, and I enjoy playing basketball, cooking, and swimming, while my favorite music is "Jesus is Lord" and my favorite food is Bicol Express; in five years, I hope to be a top software engineer.]</p>
+        <p>Hello, my name is Sean Kurtley J. Girado, and I enjoy playing basketball, cooking, and swimming, while my favorite music is "Jesus is Lord" and my favorite food is Bicol Express; in five years, I hope to be a top software engineer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
